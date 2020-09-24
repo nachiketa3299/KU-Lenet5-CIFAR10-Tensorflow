dc5a1065a588ffbd60580bbf8e7fe5935dc82c22
ncht <46415457+nachiketa3299@users.noreply.github.com>
2020-09-24 21:47:17 +0900

    일단 기본 모델 다시 만듦(아직 실행 안 해봄) 특히 conv_fn들을 모두 get_variable로 바꾸어 주어서, initialization을 편하게 할 수 있도록 했음

--- lenet.py
+++ lenet.py
@@ -21,6 +21,9 @@ import math
 '''
 class LeNet:
     def __init__(self, config):
+        # Structure definition
+        self.initializer = tf.contrib.layers.xavier_initializer()
+
         # 정답 Label 갯수 - 10개 (airplane, automobile, bird, cat, deer, dog, frog, horse, ship, truck)
         self._num_classes = config.num_classes
         # Weight Decay 위한 Lambda 값
@@ -37,34 +40,36 @@ class LeNet:
         # Input으로 들어올 데이터 사이즈 (32, 32, 3)
         # LAYER 1: (32, 32, 3) -> (28 * 28 * 6) | filter = (5 * 5 * 3) * 6
         # * hint he initialization: stddev = sqrt(2/n), filter에서 n 값은 이전 레이어의 노드 수.
-        self.conv_f1 = tf.Variable(tf.random_normal([5, 5, 3, 6], stddev=math.sqrt(2 / (32 * 32 * 3))), name='conv_f1')
-        self.C1 = tf.nn.conv2d(self.X, self.conv_f1, strides=[1, 1, 1, 1], padding='SAME')
+        # self.conv_f1 = tf.Variable(tf.random_normal([5, 5, 3, 6], stddev=math.sqrt(2 / (32 * 32 * 3))), name='conv_f1')
+        self.conv_f1 = tf.get_variable(name='conv_f1', shape=[5, 5, 3, 6], initializer=self.initializer)
+        self.C1 = tf.nn.relu(tf.nn.conv2d(self.X, self.conv_f1, strides=[1, 1, 1, 1], padding='SAME'))
 
-        # LAYER 2: (28 * 28 * 6) -> ReLu -> (28 * 28 * 6) -> max_pooling(2*2, stride=2) -> (14 * 14 * 6)
-        self.S2 = tf.nn.max_pool(tf.nn.relu(self.C1), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        # LAYER 2: (28 * 28 * 6) -> ReLU -> (28 * 28 * 6) -> max_pooling(2*2, stride=2) -> (14 * 14 * 6)
+        self.S2 = tf.nn.max_pool(self.C1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
 
         # LAYER 3: (14 * 14 * 6) -> (10 * 10 * 16) | filter = (5 * 5 * 6) * 16
-        self.conv_f2 = tf.Variable(tf.random_normal([5, 5, 6, 16], stddev=math.sqrt(2 / (14 * 14 * 6))), name='conv_f2')
-        self.C3 = tf.nn.conv2d(self.S2, self.conv_f2, strides=[1, 1, 1, 1], padding='SAME')
+        # self.conv_f2 = tf.Variable(tf.random_normal([5, 5, 6, 16], stddev=math.sqrt(2 / (14 * 14 * 6))), name='conv_f2')
+        self.conv_f2 = tf.get_variable(name='conv_f2', shape=[5, 5, 6, 16], initializer=self.initializer)
+        self.C3 = tf.nn.relu(tf.nn.conv2d(self.S2, self.conv_f2, strides=[1, 1, 1, 1], padding='SAME'))
 
-        # LAYER 4: (10 * 10 * 16) -> ReLu -> (10 * 10 * 16) -> max_pooling(2*2, stride=2) -> (5, 5, 16)
-        self.S4 = tf.nn.max_pool(tf.nn.relu(self.C3), ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        # LAYER 4: (10 * 10 * 16) -> ReLU -> (10 * 10 * 16) -> max_pooling(2*2, stride=2) -> (5, 5, 16)
+        self.S4 = tf.nn.max_pool(self.C3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
         # 평탄화 -> (5 * 5 *16)
 
         # LAYER 5(FC1): (5, 5, 16) -> (5 * 5 * 16 = 400) | (batch_size, 400) * (400, 120) -> (batch_size, 120)
         # FC1 추가 (5 * 5 * 16, 120) -> (120)
         self.FC1 = tf.reshape(self.S4, [config.batch_size, 5, 5, 16], name="FC1")
-        self.FCW1 = tf.get_variable("FCW1", shape=[5 * 5 * 16, 120])
+        self.FCW1 = tf.get_variable(name="FCW1", shape=[5 * 5 * 16, 120], initializer=self.initializer)
         self.FCB1 = tf.Variable(tf.random_normal([120]), name="FCB1")
 
         # LAYER 6(FC2): (batch_size, 120) * (120, 84) -> (batch_size, 84)
         self.FC2 = tf.nn.dropout(tf.matmul(self.FC1, self.FCW1) + self.FCB1, keep_prob=config.keep_prob)
-        self.FCW2 = tf.get_variable("FCW2", shape=[120, 84])
+        self.FCW2 = tf.get_variable(name="FCW2", shape=[120, 84], initializer=self.initializer)
         self.FCB2 = tf.Variable(tf.random_normal([84]), name="FCB2")
 
         # LAYER 7(Softmax): (batch_size, 84) * (84, 10) -> softmax -> (batch_size, 10)
         self.FC3 = tf.nn.dropout(tf.matmul(self.FC2, self.FCW2) + self.FCB2, keep_prob=config.keep_prob)
-        self.FCW3 = tf.get_variable("FCW3", shape=[84, 10])
+        self.FCW3 = tf.get_variable(name="FCW3", shape=[84, 10], initializer=self.initializer)
         self.FCB3 = tf.Variable(tf.random_normal([10]), name="FCB3")
         hypothesis = tf.nn.softmax(tf.nn.xw_plus_b(self.FC3, self.FCW3, self.FCB3, name="hypothesis"))
 
